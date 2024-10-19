982f50800b5771e98566cd5c696fd5ee5f98d579
Mihika-Grover <mihika.grover_ug2023@ashoka.edu.in>
2024-10-19 20:39:14 +0530

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -164,3 +164,36 @@ public:
 
 
 
+int main() {
+    int initialSize = 7; 
+    HashTable ht(initialSize);
+
+    // Example test case
+    ht.insert(1);
+    ht.printTable();
+    ht.insert(6);
+    ht.printTable();
+    ht.insert(15);
+    ht.printTable(); 
+    ht.insert(25);
+    ht.printTable();
+    ht.remove(15);
+    ht.printTable();
+    ht.insert(29);  
+    ht.printTable(); 
+    
+    ht.insert(35);  
+    ht.printTable();
+    ht.insert(42);  
+    ht.printTable();
+    ht.insert(11);  
+    ht.printTable();
+    ht.insert(2);  
+    ht.printTable();
+
+    int find = ht.search(22);
+    std::cout << "Found at:" << find << std::endl;
+
+    return 0;
+}
+
